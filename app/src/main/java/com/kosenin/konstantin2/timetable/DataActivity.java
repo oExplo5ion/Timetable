cdc919b101c9fdeb8b4ac3c37e09fa445b370ea0
Axel Rogers <axelviruslive@gmail.com>
2017-03-13 17:24:59 +0200

    navigation through DB added

--- app/src/main/java/com/kosenin/konstantin2/timetable/DataActivity.java
+++ app/src/main/java/com/kosenin/konstantin2/timetable/DataActivity.java
@@ -1,13 +1,26 @@
 package com.kosenin.konstantin2.timetable;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.kosenin.konstantin2.timetable.classes.FireBaseHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataActivity extends AppCompatActivity {
 
@@ -25,29 +38,86 @@ public class DataActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // accept data from intent
         String data = getIntent().getExtras().getString("data");
-        Type type = (Type) getIntent().getExtras().get("type");
+        final Type type = (Type) getIntent().getExtras().get("type");
+
+        // switch data
+        DatabaseReference mTypeBDRef;
+        Type callType = null;
 
         switch (type){
             case UNN:
-                // load unn
-                System.out.print("ezzz");
+                mTypeBDRef = FireBaseHelper.getUNN();
+                callType = Type.FACULTY;
                 break;
             case FACULTY:
-                // load faculty
+                mTypeBDRef = FireBaseHelper.getFaculty();
+                callType = Type.RGF;
                 break;
             case RGF:
-                // load rgf
+                mTypeBDRef = FireBaseHelper.getRGF();
                 break;
             default:
+                mTypeBDRef = FireBaseHelper.getUniversity();
+                callType = Type.UNN;
                 break;
         }
+
+        // collect data
+        final List<String> lst = new ArrayList<>();
+        final ListView listView = (ListView) findViewById(R.id.list_v);
+
+        final Type finalCallType = callType;
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (type != Type.RGF) {
+                    Intent intent = new Intent(DataActivity.this, DataActivity.class);
+                    intent.putExtra("data", lst.get(position));
+                    intent.putExtra("type", finalCallType);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        ChildEventListener mTypeChildEventListener = new ChildEventListener() {
+             @Override
+             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                 lst.add(dataSnapshot.getKey());
+             }
+
+             @Override
+             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+             }
+
+             @Override
+             public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+             }
+
+             @Override
+             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+             }
+
+             @Override
+             public void onCancelled(DatabaseError databaseError) {
+
+             }
+         };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(DataActivity.this, android.R.layout.simple_list_item_1, lst);
+        listView.setAdapter(adapter);
+        mTypeBDRef.addChildEventListener(mTypeChildEventListener);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
@@ -55,6 +125,8 @@ public class DataActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
             case R.id.log_out:
                 AuthUI.getInstance().signOut(this);
                 break;
