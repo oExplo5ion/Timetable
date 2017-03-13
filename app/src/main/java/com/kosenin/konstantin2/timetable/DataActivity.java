e883343a49881fd3f45a74759eb2184146412889
Axel Rogers <axelviruslive@gmail.com>
2017-03-13 16:04:51 +0200

    back button added

--- app/src/main/java/com/kosenin/konstantin2/timetable/DataActivity.java
+++ app/src/main/java/com/kosenin/konstantin2/timetable/DataActivity.java
@@ -3,17 +3,27 @@ package com.kosenin.konstantin2.timetable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.firebase.ui.auth.AuthUI;
 
 public class DataActivity extends AppCompatActivity {
 
     enum Type{
-        UNN, FACULTY, RGF;
+        UNN, FACULTY, RGF
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.data_activity);
+
+        // action bar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         String data = getIntent().getExtras().getString("data");
         Type type = (Type) getIntent().getExtras().get("type");
@@ -34,4 +44,22 @@ public class DataActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.log_out:
+                AuthUI.getInstance().signOut(this);
+                break;
+        }
+        return true;
+    }
+
 }
